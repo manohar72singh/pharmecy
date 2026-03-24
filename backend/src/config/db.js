3b1130c725718707ff311efd2d87b9e6dccb3ed5
Manohar Singh <manohar72singh@gmail.com>
2026-03-25 03:08:04 +0530

    change config file

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,15 +1,20 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
-
+import fs from "fs";
+import path from "path";
 dotenv.config();
-
+const cpath = path.resolve("./ca.pem");
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
+  port: 4000,
   connectionLimit: 10,
+  ssl: {
+    ca: fs.readFileSync(cpath),
+  },
 });
 
 export default pool;

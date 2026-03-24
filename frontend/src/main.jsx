9e857ed837be2b95313fc6d0e08892aec6c1abb9
Manohar Singh <manohar72singh@gmail.com>
2026-03-25 03:34:56 +0530

    change File frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ToastProvider } from "./context/ToastContext.jsx";
+import { ToastProvider } from "./context/Toastcontext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
